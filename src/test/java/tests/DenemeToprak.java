9f55621422c6bb715bb1d3bd1ba3e2f02c19974e
toprak <toprakyemelek1995@gmail.com>
2024-06-12 19:34:26 +0300

    v1

--- src/test/java/tests/DenemeToprak.java
+++ src/test/java/tests/DenemeToprak.java
@@ -25,7 +25,27 @@ public class DenemeToprak {
 
         }
 
+    }
+
+    @Test
+    public void toprakupdate() throws SQLException {
+
+        String sql = "INSERT INTO admins (name,email,username,password) VALUES (?,?,?,?)";
+
+        Connection connection = DriverManager.getConnection(ConnectionManage.URL,ConnectionManage.USERNAME,ConnectionManage.PASSWORD);
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        preparedStatement.setString(1,"Toprak");
+        preparedStatement.setString(2,"toprak1@gmail.com");
+        preparedStatement.setString(3,"Toprak1");
+        preparedStatement.setString(4,"toprak");
+
+        System.out.println(preparedStatement.executeUpdate());
+        preparedStatement.close();
 
 
     }
+
+
 }
