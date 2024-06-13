db1d0e613ac855f12fb58c31b974da501b284672
Esra Celik <esracelik4623@gmail.com>
2024-06-13 14:14:05 +0200

    esra

--- src/test/java/tests/DenemeEsra.java
+++ src/test/java/tests/DenemeEsra.java
@@ -1,6 +1,7 @@
 package tests;
 
 import manage.ConnectionManage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
 
@@ -43,6 +44,42 @@ public class DenemeEsra {
         preparedStatement.setInt(2,2);
 
         System.out.println(preparedStatement.executeUpdate());
+
+
+    }
+
+    @Test
+    public void deleteAylin() throws SQLException {
+
+        // delete from gateways where id = 57;
+
+        // connection
+
+        Connection connection = DriverManager.getConnection(ConnectionManage.URL,ConnectionManage.USERNAME, ConnectionManage.PASSWORD);
+
+        // statement query
+
+        String deleteQuery = "delete from gateways where id = ? ";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(deleteQuery);
+
+        int id = 57;
+
+        preparedStatement.setInt(1,id);
+
+        System.out.println(preparedStatement.executeUpdate()); // Bu bize int bir veri dondurecek, etkilenen satir sayisini dondurecek
+
+        String controlQuery = "select * from gateways where id = ? "; // Bu da bize result set dondurecek
+
+        PreparedStatement preparedStatement1 = connection.prepareStatement(controlQuery);
+
+        preparedStatement1.setInt(1,id);
+
+        ResultSet resultSet = preparedStatement1.executeQuery();
+
+        Assert.assertFalse(resultSet.next());
+
+
 
 
     }
